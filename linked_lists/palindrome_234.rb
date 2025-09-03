a99534ff4d5836764333f87744d68f44d3c8f6d0
jbk2 <james@humanbenefits.com>
2025-09-03 11:54:45 +0200

    complete palindrome_234

--- linked_lists/palindrome_234.rb
+++ linked_lists/palindrome_234.rb
@@ -16,8 +16,8 @@ module Palindrome
     return true if head.nil? || head.next.nil?
 
     orig_head = head
-    fast = orig_head
-    slow = orig_head
+    fast = orig_head # detect end
+    slow = orig_head # when fast at end this is either at middle (if odd # of nodes) or end of 1st half (if even # of nodes)
 
     while fast.next&.next
       fast = fast.next.next
@@ -25,27 +25,23 @@ module Palindrome
     end
 
     half1 = head
-    half2 = reverse_list(slow.next)
-    slow.next = half2
+    half2 = slow.next = reverse_list(slow.next)
 
     while half1 && half2
       if half1.val != half2.val
-        slow.next = reverse_list(half2)
-        return false 
+        slow.next = reverse_list(slow.next)
+        return false
+        # head.inspect
       end
 
       half1 = half1.next
       half2 = half2.next
     end
 
-    slow.next = reverse_list(half2)
-    true
+    slow.next = reverse_list(slow.next)
+    return true
+    # head.inspect
   end
-
-  # module_function :is_palindrome?
-  # private_class_method :reverse_list
-
-  # private
 
   def reverse_list(head)
     prev = nil
@@ -66,11 +62,16 @@ include Palindrome
 
 test_arr1 = [1, 2, 2, 1]
 test_arr2 = [1, 2]
+test_arr3 = [1, 2, 3, 3, 2, 1]
+test_arr4 = [1, 2, 3, 3, 2, 2, 1]
 test_ll1 = create_linked_list(test_arr1)
 test_ll2 = create_linked_list(test_arr2)
-
+test_ll3 = create_linked_list(test_arr3)
+test_ll4 = create_linked_list(test_arr4)
 # puts test_ll1.inspect
 # puts test_ll2.inspect
 
-puts is_palindrome?(test_ll1)
-puts is_palindrome?(test_ll2)
+is_palindrome?(test_ll1) ? (puts "✅ test_ll1 passes") : (puts "❌ test_ll1 fails")
+is_palindrome?(test_ll2) ? (puts "❌ test_ll2 fails") : (puts "✅ test_ll2 passes")
+is_palindrome?(test_ll3) ? (puts "✅ test_ll3 passes") : (puts "❌ test_ll3 fails")
+is_palindrome?(test_ll4) ? (puts "❌ test_ll4 fails") : (puts "✅ test_ll4 passes")
