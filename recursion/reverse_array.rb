c263b3cd63905c468e530f5da6a017c19e3bef84
jbk2 <james@humanbenefits.com>
2025-09-07 15:16:59 +0200

    adapt reverse_array to use recursion

--- recursion/reverse_array.rb
+++ recursion/reverse_array.rb
@@ -1,14 +1,17 @@
 # @param {Character[]} s
 # @return {Void} Do not return anything, modify s in-place instead.
 def reverse_string(s)
-  reverser(s, 0, s.length - 1)
-  s
-end
+  rec = -> (l, r) do
+    s[l], s[r] = s[r], s[l]
+  end
 
-def reverser(s, l_ind, r_ind)
-  s[l_ind], s[r_ind] = s[r_ind], s[l_ind]
-  l_ind += 1
-  r_ind -= 1
-  reverser(s, l_ind, r_ind) unless l_ind >= r_ind
-  
+  l, r = 0, s.length - 1
+
+  while l < r
+    rec.call(l, r)
+    l += 1
+    r -= 1
+  end
+
+  s
 end
