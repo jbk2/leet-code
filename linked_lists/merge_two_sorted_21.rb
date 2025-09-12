04c8f968bc754d5b649ab33387964e3abe7e1507
jbk2 <james@humanbenefits.com>
2025-09-12 17:36:28 +0200

    write recursive version of merge_two_sorted_21

--- linked_lists/merge_two_sorted_21.rb
+++ linked_lists/merge_two_sorted_21.rb
@@ -1,3 +1,5 @@
+require_relative './linked_list_helper'
+
 # Definition for singly-linked list.
 # class ListNode
 #     attr_accessor :val, :next
@@ -9,28 +11,72 @@
 # @param {ListNode} list1
 # @param {ListNode} list2
 # @return {ListNode}
-def merge_two_lists(list1, list2)
-  return list1 || list2 unless list1 && list2
+# def merge_two_lists(list1, list2)
+#   return list1 || list2 unless list1 && list2
 
-  list2_larger = list1.val <= list2.val
-  head = tail = list2_larger ? list1 : list2
-  list2_larger ? list1 = list1.next : list2 = list2.next
+#   list2_larger = list1.val <= list2.val
+#   head = tail = list2_larger ? list1 : list2
+#   list2_larger ? list1 = list1.next : list2 = list2.next
 
-  while list1 && list2
-    list2_larger = list1.val <= list2.val
+#   while list1 && list2
+#     list2_larger = list1.val <= list2.val
 
-    if list2_larger
-      tail.next = list1
-      list1 = list1.next
-    else
-      tail.next = list2
-      list2 = list2.next
-    end
+#     if list2_larger
+#       tail.next = list1
+#       list1 = list1.next
+#     else
+#       tail.next = list2
+#       list2 = list2.next
+#     end
 
-    tail = tail.next
+#     tail = tail.next
+#   end
+
+#   tail.next = list1 || list2
+
+#   head
+# end
+
+# def merge_two_lists(list1, list2)
+
+#   if list1.val <= list2.val
+#     head = tail = list1
+#     list1 = list1.next
+#   else
+#     head = tail = list2
+#     list2 = list2.next
+#   end
+
+#   while list1 && list2
+#     if list1.val <= list2.val
+#       tail.next = list1
+#       list1 = list1.next
+#     else
+#       tail.next = list2
+#       list2 = list2.next
+#     end
+
+#     tail = tail.next
+#   end
+
+#   tail.next = list1 || list2
+#   head
+# end
+
+def merge_two_lists(l1, l2)
+  return l1 if l2.nil?
+  return l2 if l1.nil?
+
+  if l1.val <= l2.val
+    l1.next = merge_two_lists(l1.next, l2)
+    l1
+  else
+    l2.next = merge_two_lists(l1, l2.next)
+    l2
   end
-
-  tail.next = list1 || list2
-
-  head
 end
+
+ll1 = build_linked_list
+ll2 = build_linked_list
+
+puts merge_two_lists(ll1, ll2).inspect
