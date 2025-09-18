be32f595ce7eec8e2540d280258d68cd7eb902cc
jbk2 <james@humanbenefits.com>
2025-09-18 16:30:57 +0200

    Add alternative approaches to climbing_stairs Leet#70

--- recursion/climbing_stairs_70.rb
+++ recursion/climbing_stairs_70.rb
@@ -1,11 +1,32 @@
 # @param {Integer} n
 # @return {Integer}
 
-def climb_stairs(n, memo = {})
-  return 0 if n.zero?
-  return n if n < 3
+# def climb_stairs(n, memo = {})
+#   return 0 if n.zero?
+#   return n if n < 3
 
-  memo[n] ||= climb_stairs(n - 1, memo) + climb_stairs(n - 2, memo)
+#   memo[n] ||= climb_stairs(n - 1, memo) + climb_stairs(n - 2, memo)
+# end
+
+# top down
+# def climb_stairs(n, memo = [])
+#   return 0 if n.zero?
+#   return n if n < 3
+
+#   memo[n] ||= climb_stairs(n - 1, memo) + climb_stairs(n - 2, memo)
+# end
+
+# bottom up
+def climb_stairs(n)
+  memo = []
+  memo[1] = 1
+  memo[2] = 2
+
+  (3..n).each do |i|
+    memo[i] = memo[i - 1] + memo[i - 2]
+  end
+
+  memo[n]
 end
 
 puts climb_stairs(5)
