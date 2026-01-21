8ed401c85840f7736805d848338fe0a8644b4cd4
jbk2 <james@humanbenefits.com>
2026-01-21 21:38:03 +0100

    two_sum_1.rb completed

--- hashes/two_sum_1.rb
+++ hashes/two_sum_1.rb
@@ -14,7 +14,7 @@ def two_sum(nums, target)
   nums.each_with_index do |num, index|
     compliment = target - num
     if nums_indexes[compliment]
-      return [index, num_indexes[compliment]]
+      return [nums_indexes[compliment], index]
     else
       nums_indexes[num] = index
     end
@@ -24,19 +24,23 @@ end
 # Example 1:
 nums_1 = [2,7,11,15]
 target_1 = 9
-answer_1: [0,1]
+result_1 = [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
+test_1 = two_sum(nums_1, target_1)
+puts test_1 == result_1 ? "✅passes, with result = #{test_1}" : "❌fails with result = #{test_1}"
 
 # Example 2:
 nums_2 = [3,2,4]
 target_2 = 6
-answer_2 = [1,2]
-
+result_2 = [1,2]
+test_2 = two_sum(nums_2, target_2)
+puts test_2 == result_2 ? "✅passes, with result = #{test_2}" : "❌fails with result = #{test_2}"
 # Example 3:
 nums_3 = [3,3]
 target_3 = 6
-answe_3 = [0,1]
+result_3 = [0,1]
+test_3 = two_sum(nums_3, target_3)
+puts test_3 == result_3 ? "✅passes, with result = #{test_3}" : "❌fails with result = #{test_3}"
  
 
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
