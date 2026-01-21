17726993a89c38d62eb42afa5359d0acee73edf5
jbk2 <james@humanbenefits.com>
2026-01-21 21:38:22 +0100

    create folder for JSON filtering play

--- dp/job_difficulty_1335.rb
+++ dp/job_difficulty_1335.rb
@@ -33,4 +33,3 @@ test3 = min_difficulty(job_difficulty3, d3)
 puts test1 == result1 ? "✅passes, with job_difficulty;#{job_difficulty1} & d;#{d1}, result = #{test1}" : "❌fails with withjob_difficulty;#{job_difficulty2} & d;#{d1}, result = #{test1}"
 puts test2 == result2 ? "✅passes, with job_difficulty;#{job_difficulty2} & d;#{d2}, result = #{test2}" : "❌fails with withjob_difficulty;#{job_difficulty1} & d;#{d2}, result = #{test2}"
 puts test3 == result3 ? "✅passes, with job_difficulty;#{job_difficulty3} & d;#{d3}, result = #{test3}" : "❌fails with withjob_difficulty;#{job_difficulty3} & d;#{d3}, result = #{test3}"
-
