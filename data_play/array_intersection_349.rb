a59e433321bcf956a3173d14e16c2c1d619ccaa7
jbk2 <james@humanbenefits.com>
2026-01-25 18:16:06 +0100

    manual implementation of array_intersection #349 with bubble sort & manual two pointers

--- data_play/array_intersection_349.rb
+++ data_play/array_intersection_349.rb
@@ -1,7 +1,8 @@
-# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique
+# and you may return the result in any order.
 
 def intersection_easy(arr1, arr2)
-  arr1.intersection(arr2)
+  arr1.intersection1(arr2)
 end
 
 def intersection(arr1, arr2)
@@ -16,13 +17,53 @@ def intersection(arr1, arr2)
   intersections.uniq
 end
 
+def intersection2(arr1, arr2)
+  bubble_sort(arr1)
+  bubble_sort(arr2)
+  
+  same = ->(a, b) { a == b }
+  commons = Set.new
+  i1, i2 = 0, 0
+
+  while i1 < arr1.length && i2 < arr2.length
+    if arr1[i1] == arr2[i2]
+      commons.add(arr1[i1])
+      i1 += 1; i2 += 1
+    elsif arr1[i1] < arr2[i2]
+      i1 += 1
+    else
+      i2 += 1
+    end
+  end
+
+  commons.to_a
+end
+
+def bubble_sort(arr)
+  loop do
+    all_sorted = true
+
+    (arr.length - 1).times do |idx|
+      # while arr[idx + 1]
+      if arr[idx] <= arr[idx + 1]
+        next
+      else
+        all_sorted = false
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+      end
+    end
+    break if all_sorted
+  end
+  arr
+end
+
 
 # Example 1:
 nums1a = [1,2,2,1]
 nums1b = [2,2]
 result_1 = [2]
 
-output_1 = intersection(nums1a, nums1b)
+output_1 = intersection2(nums1a, nums1b)
 puts output_1 == result_1 ? "✅passes - intersection vals are #{output_1}"
   : "❌fails - result is #{output_1}, but should be #{result_1}"
 
@@ -32,7 +73,7 @@ nums2b = [9,4,9,8,4]
 result_2 = [4,9]
 # Explanation: [4,9] is also accepted.
 
-output_2 = intersection(nums2a, nums2b)
+output_2 = intersection2(nums2a, nums2b)
 puts output_2 == result_2 ? "✅passes - intersection vals are #{output_2}"
   : "❌fails - result is #{output_2}, but should be #{result_2}"
 
