359dcc997b243d8b1a1562b186eb77cb53a3f94b
jbk2 <james@humanbenefits.com>
2026-02-02 17:42:46 +0100

    Add more test cases to prerequisite_courses.rb & a refactor to return left middle on even length input

--- dsa_interview_qs/prerequisite_courses.rb
+++ dsa_interview_qs/prerequisite_courses.rb
@@ -54,7 +54,7 @@ def mid_course(input)
   ll_head = course_linked_list(input)
   slow, fast = ll_head, ll_head
   
-  while fast && fast.nxt
+  while fast&.nxt&.nxt
     slow = slow.nxt
     fast = fast.nxt.nxt
   end
@@ -72,7 +72,39 @@ input_1 = [
   ["Software Design", "Computer Networks"]
 ]
 answer_1 = "Data Structures"
-
 output_1 = mid_course(input_1)
 puts output_1 == answer_1 ? "✅passes - with the correct mid course of; #{output_1}"
-  : "❌fails - mid course shoudl be #{answer_1}, but code returned #{output_1}"
+: "❌fails - mid course shoudl be #{answer_1}, but code returned #{output_1}"
+
+# Example 2:
+input_2 = [
+  ["Math", "Physics"]
+]
+answer_2 = "Math"
+output_2 = mid_course(input_2)
+puts output_2 == answer_2 ? "✅passes - with the correct mid course of; #{output_2}"
+: "❌fails - mid course shoudl be #{answer_2}, but code returned #{output_2}"
+
+# Example 3:
+input_3 = [
+  ["Intro", "Intermediate"],
+  ["Intermediate", "Advanced"]
+]
+
+answer_3 = "Intermediate"
+output_3 = mid_course(input_3)
+puts output_3 == answer_3 ? "✅passes - with the correct mid course of; #{output_3}"
+: "❌fails - mid course shoudl be #{answer_3}, but code returned #{output_3}"
+
+# Example 4:
+input_4 = [
+  ["E", "F"],
+  ["A", "B"],
+  ["D", "E"],
+  ["C", "D"],
+  ["B", "C"]
+]
+answer_4 = "C"
+output_4 = mid_course(input_4)
+puts output_4 == answer_4 ? "✅passes - with the correct mid course of; #{output_4}"
+: "❌fails - mid course shoudl be #{answer_4}, but code returned #{output_4}"
