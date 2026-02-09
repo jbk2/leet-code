a7bcc762fce3a299718839808d7238e84d3ef9ed
jbk2 <james@humanbenefits.com>
2026-02-09 17:20:51 +0100

    scaffold out a binary heap class

---
+++ sorting/heap.rb
@@ -0,0 +1,27 @@
+class Heap
+  attr_accessor :root
+  
+  def initialize(arr)
+    build_heap(arr)
+  end
+
+  
+
+  def parent(index)
+    
+  end
+
+  def left(index)
+    
+  end
+
+  def right(index)
+    
+  end
+
+  private
+  def build_heap(arr)
+    
+  end
+
+end
