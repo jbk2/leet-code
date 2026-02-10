e744b64b11e8a83621af68234cb2396403d5cbdb
jbk2 <james@humanbenefits.com>
2026-02-10 09:32:13 +0100

    Further scaffold MaxHeap in heap.rb

--- sorting/heap.rb
+++ sorting/heap.rb
@@ -1,27 +1,48 @@
-class Heap
-  attr_accessor :root
+class MaxHeap
+  attr_reader :heap_arr
   
   def initialize(arr)
-    build_heap(arr)
+    @heap_arr = build_heap(arr)
+  end
+  
+  def max
+    @heap_arr[0]
   end
 
-  
+  def insert
+    # add to end of heap_arr
+    # calc parent
+    # if larger than parent then swap (sift up or sift down)- repeat until in order
+  end
+
+  def sift_up(index)
+    
+  end
+
+  def sift_down(index)
+    
+  end
 
   def parent(index)
-    
+    (index - 1) / 2
   end
 
   def left(index)
-    
+    2 * index + 1
   end
 
   def right(index)
+    2 * index + 2
+  end
+
+  def max_heapify
     
   end
 
   private
   def build_heap(arr)
-    
+    # build the arr of the heap
+    []
   end
 
 end
