d501cb5890fae7e79d146d40639b149645e93f1d
jbk2 <james@humanbenefits.com>
2026-02-18 15:24:07 +0100

    further merge-sort.js tests

--- sorting/merge_sort/merge-sort.test.js
+++ sorting/merge_sort/merge-sort.test.js
@@ -6,5 +6,17 @@ describe('mergeSort function', () => {
     const test_arr = [1, 7, 2, 5, 10, 9];
     const result = [1, 2, 5, 7, 9, 10];
     expect(mergeSort(test_arr)).toEqual(result);
-  })
+  });
+  
+  test('given an unsorted array of 10 it returns the sorted array', () => {
+    const test_arr = [1, 7, 2, 5, 10, 9, 10, 11, 289, 3];
+    const result = [1, 2, 3, 5, 7, 9, 10, 10, 11, 289];
+    expect(mergeSort(test_arr)).toEqual(result);
+  });
+  
+  test('given an unsorted array of 6 it returns the sorted array', () => {
+    const test_arr = [1, 2, 5, 7, 9, 10];
+    const result = [1, 2, 5, 7, 9, 10];
+    expect(mergeSort(test_arr)).toEqual(result);
+  });
 })
