17b9c25b7a0e52662bea4b0f6324760103000de2
jbk2 <james@humanbenefits.com>
2026-04-04 14:28:40 +0200

    Write & test functionality to parse csv group date into ruby objects

--- interview_qs/takehome_test/lib/multipart_grouper.rb
+++ interview_qs/takehome_test/lib/multipart_grouper.rb
@@ -1,9 +1,13 @@
 require "csv"
 
+# build the three groups into three sets from the csvs
+# 
+
 class MultipartGrouper
   def initialize(input_file:, group_files:)
     @input_file = input_file
     @group_files = group_files
+    @study_groups = group_csvs
   end
 
   # Implement this method
@@ -25,15 +29,17 @@ class MultipartGrouper
   end
 
   def group_csvs
-    @group_csvs ||=
-      @group_files.map do |group_file|
-        [
-          group_file.split("/").last,
-          CSV.read(
-            group_file,
-            headers: true
-          )
-        ]
+    groups = {}
+    @group_files.each do |file_path|
+      study_name = File.basename(file_path, '.csv').tr(' ', '_').downcase.to_sym
+      groups[study_name] = []
+      # file_contents = File.read(file_path)
+      CSV.foreach(file_path, headers: true) do |row|
+        groups[study_name] << row["Study Name"]
       end
+      groups[study_name].sort!
+    end
+    groups
   end
 end
+
--- interview_qs/takehome_test/spec/multipart_grouper_spec.rb
+++ interview_qs/takehome_test/spec/multipart_grouper_spec.rb
@@ -2,17 +2,30 @@ require "spec_helper"
 require_relative "../lib/multipart_grouper"
 
 RSpec.describe MultipartGrouper do
-  let(:group_paths) { Dir.glob("./spec/fixtures/groups/*.csv") }
+  let(:group_paths) { Dir.glob("./spec/fixtures/groups/MRI*.csv") }
   let(:input_path) { "./spec/fixtures/inputs/#{input_pathname}.csv" }
 
   subject do
-    MultipartGrouper.new(input_file: input_path, group_files: group_paths).group
+    MultipartGrouper.new(input_file: input_path, group_files: group_paths)
   end
 
+  context "parsing the groups" do
+    let(:input_pathname) { "test_one" }
+    it ":group_csvs parses the groups CSVs contents into array of ruby objects" do
+      group_objs = { mri_axial_skeleton: ["MRI Cervical Spine","MRI Hip Both","MRI Lumbar Spine","MRI Pelvis","MRI SIJ Both","MRI Spine Coccyx","MRI Spine Lumbar/Sacral","MRI Spine Sacrum","MRI Spine Thoracic"],
+        mri_lower_limb: ["MRI Ankle","MRI Femur","MRI Foot","MRI Hip","MRI Knee","MRI Lower Leg","MRI Lumbar Spine","MRI Pelvis","MRI SIJ","MRI SIJ Both","MRI Spine Coccyx","MRI Spine Sacrum","MRI Thigh"],
+        mri_upper_limb: ["MRI Brachial Plexus","MRI Cervical Spine","MRI Elbow","MRI Fingers","MRI Forearm","MRI Hand","MRI Radius & Ulnar","MRI Scaphoid","MRI Scapula","MRI Shoulder","MRI Thumb","MRI Upper Arm","MRI Wrist"]
+      }
+
+      expect(subject.send(:group_csvs)).to eq(group_objs)
+    end
+    
+  end
+  
   context "finds correct groups of two two and two" do
     let(:input_pathname) { "solved_example" }
 
-    it "creates the expected mapping of 3 groups of 2 studies" do
+    xit "creates the expected mapping of 3 groups of 2 studies" do
       expect(subject).to eq(
         [
           ["MRI Elbow", "MRI Forearm"],               # Created from the MRI Upper Limb group
@@ -26,7 +39,7 @@ RSpec.describe MultipartGrouper do
   context "Test input group one" do
     let(:input_pathname) { "test_one" }
 
-    it "finds correct groups of two and four studies with no remainders" do
+    xit "finds correct groups of two and four studies with no remainders" do
       expect(subject).to eq(
         [
           [
@@ -47,7 +60,7 @@ RSpec.describe MultipartGrouper do
   context "Test input group two" do
     let(:input_pathname) { "test_two" }
 
-    it "finds the expect group of 5, the one remainder is not included as it is not a group" do
+    xit "finds the expect group of 5, the one remainder is not included as it is not a group" do
       expect(subject).to eq(
         [
           [
@@ -65,7 +78,7 @@ RSpec.describe MultipartGrouper do
   context "Test input group three" do
     let(:input_pathname) { "test_three" }
 
-    it "creates the expected groups of six and two studies with no remainders" do
+    xit "creates the expected groups of six and two studies with no remainders" do
       expect(subject).to eq(
         [
           [
