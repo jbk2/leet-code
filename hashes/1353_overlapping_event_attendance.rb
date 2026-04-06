a94a28c1794ef8be504de2743dc3602dc183606b
jbk2 <james@humanbenefits.com>
2026-04-06 12:24:20 +0200

    Further improve #1353

--- hashes/1353_overlapping_event_attendance.rb
+++ hashes/1353_overlapping_event_attendance.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # You are given an array of events where events[i] = [startDayi, endDayi]. Every event i starts at startDayi and ends at endDayi.
 # You can attend an event i at any day d where startDayi <= d <= endDayi. You can only attend one event at any time d.
 # Return the maximum number of events you can attend.
@@ -6,26 +7,25 @@
 # 1 <= events.length <= 105
 # events[i].length == 2
 # 1 <= startDayi <= endDayi <= 105
- 
 
 def max_attendances(events)
-  attendable_event_count = 0
   ordered_events = events.sort_by { |start_day, end_day| [end_day, start_day] }
-  min_day, max_day = events.flatten.min, events.flatten.max
-  days_taken = (min_day..max_day).to_h { |day| [day, false] }
+  days_taken = Hash.new()
+  attendable_event_count = 0
 
   ordered_events.each do |start_day, end_day|
-    (start_day..end_day).each do |day|
-      if days_taken[day] == false
-        attendable_event_count += 1
-        days_taken[day] = true
-        break
-      end
-    end
+    attendable_day = (start_day..end_day).find { |day| !days_taken.has_key?(day) }
+    next unless attendable_day
+    attendable_event_count += 1
+    days_taken[attendable_day] = true
   end
 
   attendable_event_count
 end
+
+events = [[1,2],[2,3],[3,4]]
+answer = 3
+puts max_attendances(events)
 
 RSpec.describe ":max_attendances" do
   describe "test 1" do
