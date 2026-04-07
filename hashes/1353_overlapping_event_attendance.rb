75413f3b9e47aba30b7c59f01e31c11480340a80
jbk2 <james@humanbenefits.com>
2026-04-07 11:48:03 +0200

    Complete #1353

--- hashes/1353_overlapping_event_attendance.rb
+++ hashes/1353_overlapping_event_attendance.rb
@@ -8,7 +8,56 @@ require 'byebug'
 # events[i].length == 2
 # 1 <= startDayi <= endDayi <= 105
 
-def max_attendances(events)
+def max_attendances_3(events)
+  ordered_events = events.sort_by { |start_day, end_day| [end_day, start_day] }
+  max_day = events.flatten.max
+  calendar = (0..max_day + 1).to_a
+  attendable_event_count = 0
+  
+  first_free_from = lambda do |day|
+    return max_day + 1 if day > max_day
+
+    if calendar[day] != day
+      calendar[day] = first_free_from.call(calendar[day])
+    end
+
+    calendar[day]
+  end
+  
+  ordered_events.each do |start_day, end_day|
+    available_day = first_free_from.call(start_day)
+    next unless available_day <= end_day
+    calendar[available_day] = first_free_from.call(available_day + 1)
+    attendable_event_count += 1
+  end
+  
+  attendable_event_count
+end
+
+def max_attendances_1(events)
+  ordered_events = events.sort_by { |start_day, end_day| [end_day, start_day] }
+  max_day = events.flatten.max
+  calendar = (0..max_day + 1).to_a
+  attendable_event_count = 0
+  
+  first_free_from = lambda do |day|
+    while calendar[day] != day
+      day = calendar[day]
+    end
+    day
+  end
+  
+  ordered_events.each do |start_day, end_day|
+    available_day = first_free_from.call(start_day)
+    next unless available_day <= end_day
+    calendar[available_day] = first_free_from.call(available_day + 1)
+    attendable_event_count += 1
+  end
+  
+  attendable_event_count
+end
+
+def max_attendances_1(events)
   ordered_events = events.sort_by { |start_day, end_day| [end_day, start_day] }
   days_taken = Hash.new()
   attendable_event_count = 0
@@ -25,7 +74,7 @@ end
 
 events = [[1,2],[2,3],[3,4]]
 answer = 3
-puts max_attendances(events)
+puts max_attendances_3(events)
 
 RSpec.describe ":max_attendances" do
   describe "test 1" do
@@ -38,7 +87,7 @@ RSpec.describe ":max_attendances" do
     it "returns correct val" do
       events = [[1,2],[2,3],[3,4]]
       answer = 3
-      output = max_attendances(events)
+      output = max_attendances_3(events)
       expect(output).to eq(answer)
     end
   end
@@ -47,7 +96,7 @@ RSpec.describe ":max_attendances" do
     it "returns correct val" do
       events = [[1,2],[2,3],[3,4],[1,2]]
       answer = 4
-      output = max_attendances(events)
+      output = max_attendances_3(events)
       expect(output).to eq(answer)
     end
   end
