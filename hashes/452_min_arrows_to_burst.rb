0aee768efa9813de0cb7476e0661b3de7c831c20
jbk2 <james@humanbenefits.com>
2026-04-07 16:44:35 +0200

    Scaffold out #452 - min arrows to burst baloons

---
+++ hashes/452_min_arrows_to_burst.rb
@@ -0,0 +1,40 @@
+# There are some spherical balloons taped onto a flat wall that represents the XY-plane.
+# The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon
+# whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
+# Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis.
+# A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend.
+# There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+
+# Constraints:
+# 1 <= points.length <= 105
+# points[i].length == 2
+# -231 <= xstart < xend <= 231 - 1
+
+# Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+
+def min_arrows_to_burst(points)
+  
+end
+
+RSpec.describe ":min_arrows_to_burst" do
+  it "test 1" do
+    input = [[10,16],[2,8],[1,6],[7,12]]
+    answer = 2
+    output = min_arrows_to_burst(input)
+    expect(output).to eq(answer)
+  end
+  
+  it "test 2" do
+    input = [[1,2],[3,4],[5,6],[7,8]]
+    answer = 4
+    output = min_arrows_to_burst(input)
+    expect(output).to eq(answer)
+  end
+  
+  it "test 3" do
+    input = [[1,2],[2,3],[3,4],[4,5]]
+    answer = 2
+    output = min_arrows_to_burst(input)
+    expect(output).to eq(answer)
+  end
+end
