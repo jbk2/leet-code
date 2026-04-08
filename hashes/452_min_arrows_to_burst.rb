abe6a74d3d21afaf0b1935ac208c141940667a22
jbk2 <james@humanbenefits.com>
2026-04-08 14:52:12 +0200

    Complete #452 - grouping optimisation algo

--- hashes/452_min_arrows_to_burst.rb
+++ hashes/452_min_arrows_to_burst.rb
@@ -4,6 +4,9 @@
 # Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis.
 # A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend.
 # There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+#      -   ---   - 
+#    ----       ----
+#       --     -- 
 
 # Constraints:
 # 1 <= points.length <= 105
@@ -13,8 +16,27 @@
 # Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
 
 def min_arrows_to_burst(points)
-  
+  points.sort_by! { |start_x, end_x| end_x }
+  arrow_count = 0
+  last_x = nil
+
+  points.each do |s, e|
+    if last_x.nil? || s > last_x
+      arrow_count += 1
+      last_x = e
+    end
+  end
+
+  arrow_count
 end
+
+# Naive approach
+# want to hit locations where as many baloons on that x coord
+# once a baloon is hit, delete from x-coords
+# algo:
+# count no of baloons as any one x position
+# sheet arrow to positions with max counts then remove burst baloons
+# re-shoot at max positions - remove baloons, re-shoot until none left
 
 RSpec.describe ":min_arrows_to_burst" do
   it "test 1" do
