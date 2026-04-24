efd25474ca019f180506ca065ab0fad8e14f0709
jbk2 <james@humanbenefits.com>
2026-04-24 10:25:36 +0200

    generate rails 8 authentication & tailwindcss:install

--- interview_qs/dealer/db/schema.rb
+++ interview_qs/dealer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_23_190143) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_24_073957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,4 +24,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_23_190143) do
     t.integer "year"
     t.index ["state"], name: "index_requests_on_state"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
